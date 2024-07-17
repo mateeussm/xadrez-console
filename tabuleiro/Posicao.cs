039acdf93280137bf10470321c2af94993e97219
Mateus Silva <mateus.silva@linkedby.com.br>
2024-07-17 15:16:50 -0300

    Criado class Posicao

---
+++ tabuleiro/Posicao.cs
@@ -0,0 +1,21 @@
+namespace tabuleiro
+{
+    class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+        override public string ToString()
+        {
+            return 
+            linha
+            + "," 
+            + coluna;
+        }
+    }
+}
