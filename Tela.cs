f1189ed122c05512742638c2cca635ec985b89a2
Mateus Silva <mateus.silva@linkedby.com.br>
2024-07-18 15:18:23 -0300

    Melhorando a impressao do tabuleiro e das pecas

--- Tela.cs
+++ Tela.cs
@@ -6,9 +6,10 @@ namespace xadrez_console
     {
         public static void imprimirTabuleiro(Tabuleiro tab)
         {
-
+            
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8-i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -17,12 +18,29 @@ namespace xadrez_console
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.Write("  a b c d e f g h");
 
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else if(peca.cor == Cor.Preta)
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
         }
 
     }
