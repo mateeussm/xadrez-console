7704cb6202b3054574570c7ef9caea7601998161
Mateus Silva <mateus.silva@linkedby.com.br>
2024-07-18 16:57:15 -0300

    Adicionado mecanica de movimento de peças

--- Program.cs
+++ Program.cs
@@ -10,7 +10,16 @@ namespace xadrez_console
             try
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
-                Tela.imprimirTabuleiro(partida.tab);
+                while(!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+                    Console.Write("Origem :");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                    partida.executaMovimento(origem, destino);
+                }
 
             }
             catch (tabuleiroException ex)
--- Tela.cs
+++ Tela.cs
@@ -1,4 +1,5 @@
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -24,7 +25,16 @@ namespace xadrez_console
                 }
                 Console.WriteLine();
             }
-            Console.Write("  a b c d e f g h");
+            Console.WriteLine("  a b c d e f g h");
+
+        }
+
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1]+"");
+            return new PosicaoXadrez(coluna,linha);
 
         }
 
